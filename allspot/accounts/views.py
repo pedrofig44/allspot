afa8d1978845257dd67ec65affa0847ead7852bd
Pedro <pedrorelvao44@gmail.com>
2023-03-19 16:17:32 +0000

    forgot password feature

--- allspot/accounts/views.py
+++ allspot/accounts/views.py
@@ -1,12 +1,15 @@
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserForm
 from .models import User, UserProfile
 from django.contrib import messages, auth
 from vendor.forms import VendorForm
-from .utils import detectUser
+from .utils import detectUser, send_verification_email
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
 
 
 
@@ -50,6 +53,11 @@ def registerUser(request):
             user = User.objects.create_user(first_name = first_name, last_name=last_name, username=username, email=email, phone_number=phone_number, password=password)
             user.role = User.CUSTOMER
             user.save()
+
+            # Send verification email
+            mail_subject = 'Por favor ative a sua conta.'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
             messages.success(request, 'A tua conta foi criada com sucesso.')
 
             return redirect('registerUser')
@@ -88,6 +96,10 @@ def registerVendor(request):
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
+            # Send verification email
+            mail_subject = 'Por favor ative a sua conta.'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
             messages.success(request, 'A tua conta foi registada com sucesso. Por favor aguarde pela sua aprovação.')
             return redirect('registerVendor')
         else:
@@ -101,6 +113,22 @@ def registerVendor(request):
         'v_form': v_form,
     }
     return render(request, 'accounts/registerVendor.html', context)
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'A sua conta está ativada. Bem-Vindo.')
+        return redirect('myAccount')
+    else:
+        messages.error(request, 'O link de ativação é inválido.')
+        return redirect('myAccount')
 
 def login(request):
     if request.user.is_authenticated:
@@ -143,4 +171,59 @@ def custoDashboard(request):
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     return render (request, 'accounts/vendorDashboard.html')
+
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact=email)
+
+            #send reset password email
+            mail_subject = 'Reinicia a sua password.'
+            email_template = 'accounts/emails/reset_password_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+            messages.success(request, 'Um link para reiniciar a sua password foi enviado para o seu email.')
+            return redirect('login')
+        else:
+            messages.error(request, 'O mail não corresponde a uma conta.')
+            return redirect('login')
+
+    return render(request, 'accounts/forgot_password.html')
+
+def reset_password_validate(request, uidb64, token):
+    # validate the user by the decoding token and user pk
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request, 'Por favor reinicie a sua password.')
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'This link has been expired.')
+        return redirect('myAccount')
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            pk = request.session.get('uid')
+            user = User.objects.get(pk=pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request, 'Password foi alterada com sucesso.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Passwords não são iguais.')
+            return redirect('reset_password')
+    return render(request, 'accounts/reset_password.html')
+
 
--- allspot/allspot/settings.py
+++ allspot/allspot/settings.py
@@ -133,3 +133,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Configuração do email
+EMAIL_HOST = config('EMAIL_HOST') 
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'AllSpot - Sistemas de Reservas <allspotteste@gmail.com>'
