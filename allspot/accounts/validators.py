2d17445716278c301abdbc1207aa458e288488bd
Pedro <pedrorelvao44@gmail.com>
2023-04-01 01:13:33 +0100

    vendor profile form + validation

---
+++ allspot/accounts/validators.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+import os
+
+
+def allow_only_images_validator(value):
+    ext = os.path.splitext(value.name)[1] # cover-image.jpg
+    print(ext)
+    valid_extensions = ['.png', '.jpg', '.jpeg']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Ficheiros não suportados. Os ficheiros suportados são: ' +str(valid_extensions))
