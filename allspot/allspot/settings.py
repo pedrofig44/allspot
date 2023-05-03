5059838092eb3552bfeb50a7b4a302505339eec4
Pedro <pedrorelvao44@gmail.com>
2023-05-04 00:36:18 +0100

    restaurante profile + google geocoding

--- allspot/allspot/settings.py
+++ allspot/allspot/settings.py
@@ -56,6 +56,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -142,3 +143,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'AllSpot - Sistemas de Reservas <allspotteste@gmail.com>'
+
+GOOGLE_API_KEY = 'AIzaSyCRp5XGdoKz6wS4E6ARDbDumKbzO-_W8TY'
---
+++ allspot/static/js/custom.js
@@ -0,0 +1,69 @@
+let autocomplete;
+
+function initAutoComplete(){
+autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('id_address'),
+    {
+        types: ['geocode', 'establishment'],
+        //default in this app is "PT" - add your country code
+        componentRestrictions: {'country': ['pt']},
+    })
+// function to specify what should happen when the prediction is clicked
+autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged (){
+    var place = autocomplete.getPlace();
+
+    // User did not select the prediction. Reset the input field or alert()
+    if (!place.geometry){
+        document.getElementById('id_address').placeholder = "Start typing...";
+    }
+    else{
+        console.log('place name=>', place.name)
+    }
+    // get the address components and assign them to the fields
+    
+    var geocoder = new google.maps.Geocoder()
+    var address = document.getElementById('id_address').value 
+
+    geocoder.geocode({'address': address}, function(results, status){
+        // console.log('results=>', results)
+        // console.log('results=>', status)
+        if(status == google.maps.GeocoderStatus.OK){
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+
+            // console.log('lat=>', latitude);
+            // console.log('long=>', longitude);
+            $('#id_latitude').val(latitude);
+            $('#id_longitude').val(longitude);
+
+            $('#id_address').val(address);
+        }
+    });
+
+    console.log(place.address_components);
+    for(var i=0; i<place.address_components.length; i++){
+        for(var j=0; j<place.address_components[i].types.length; j++){
+            // get the country
+            if(place.address_components[i].types[j] == 'country'){
+                $('#id_country').val(place.address_components[i].long_name);
+            }
+            // get the district
+            if(place.address_components[i].types[j] == 'administrative_area_level_1'){
+                $('#id_district').val(place.address_components[i].long_name);
+            }
+            if(place.address_components[i].types[j] == 'locality'){
+                $('#id_concelho').val(place.address_components[i].long_name);
+            }
+            if(place.address_components[i].types[j] == 'postal_code'){
+                $('#id_codigo_postal').val(place.address_components[i].long_name);
+            }
+
+        }
+    }
+  
+
+    
+}
